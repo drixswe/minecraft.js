dfa2b3d3327820c240f01aa85514620f90690e90
Drix <mcorrochanoarroyo@gmail.com>
2025-05-26 17:01:27 +0200

    docs: add custom fonts and installation guide

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,26 +1,52 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
-});
+  integrations: [
+    starlight({
+      title: 'minecraft.js',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/drixswe/minecraft.js'
+        },
+        {
+          icon: 'discord',
+          label: 'Discord',
+          href: 'https://discord.gg/E24y7tahTa'
+        }
+      ],
+      defaultLocale: 'en',
+      locales: {
+        en: {
+          label: 'English'
+        },
+        es: {
+          label: 'Español'
+        }
+      },
+      sidebar: [
+        {
+          label: 'Getting Started',
+          autogenerate: {
+            directory: 'basics',
+          }
+        }
+      ],
+      expressiveCode: {
+        styleOverrides: {
+          codeFontFamily: 'JetBrains Mono',
+        }
+      },
+      customCss: [
+        './src/styles/custom.css',
+        '@fontsource/inter/400.css',
+        '@fontsource/inter/600.css',
+        '@fontsource/jetbrains-mono/400.css'
+      ]
+    })
+  ]
+})
